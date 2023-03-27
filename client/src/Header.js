732ea5f0ea9525c7dee959d68753bbf8620d33dd
sam chappel <m.samchappel@gmail.com>
2023-03-27 16:40:57 -0700

    working on scrollbar padding

--- client/src/Header.js
+++ client/src/Header.js
@@ -4,7 +4,8 @@ function Header() {
   return (
     <header className="header">
       <h1 className="crux">CRUX</h1>
-      <h2 className="crux-subheader"> Something witty</h2>
+      <h2 className="crux-subheader">Explore Your Vertical Limits</h2>
+      {/* <h3>The Ultimate Climbing Route Tracker</h3> */}
     </header>
   );
 }
