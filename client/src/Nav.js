e9b89294ed46934f5e61761472b42131530afb3a
sam chappel <m.samchappel@gmail.com>
2023-03-28 11:07:27 -0700

    added login form and conditional rendering for profile nav link

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -1,15 +1,33 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const user = localStorage.getItem('user'); // Replace 'user' with specific key
+    if (user) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   return (
     <div className="nav-container">
       <nav className="nav-bar">
         <Link to="/crux">Home</Link>
         <Link to="/routes">Routes</Link>
         <Link to="/submit">Add a Route</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/log_in">Log In</Link>
+        {isLoggedIn ? (
+          <>
+            <Link to="/profile">Profile</Link>
+            <Link to="/log_out" onClick={() => {
+              localStorage.removeItem('user');
+              setIsLoggedIn(false);
+            }}>Log Out</Link>
+          </>
+        ) : (
+          <Link to="/log_in">Log In/Sign Up</Link>
+        )}
       </nav>
     </div>
   );
